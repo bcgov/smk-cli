e2198630476166bb9e9e23530874ae11fb64ae5d
Benjamin Jubb <benjubb@gmail.com>
2020-07-20 13:32:00 -0700

    smk create restructured, functional

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-
-const ui = require('./lib/smk-ui/init');
-const cli = require('./lib/smk-cli/init');
+const chalk = require( 'chalk' )
 
 // get the command line, remove first two args (node exe and js location)
-var args = process.argv.slice(2);
+const args = process.argv
+const exec = args[ 1 ]
+const command = ( args[ 2 ] || '' ).toLowerCase()
+const opt = require( 'minimist' )( args.slice( 3 ) )
 
-// if the first arg exists, and it says 'ui', then we should attempt to launch the ui editor
-if(args[0] && args[0] === 'ui')
-{
-    ui.launch(args);
+// if the first arg is 'ui', then we should attempt to launch the ui editor
+if ( command == 'ui' )
+    return require( './smk-ui' )( opt )
+
+// if the first arg is 'create', then we should attempt to launch the cli project creator
+if ( command == 'create' )
+    return require( './smk-create' )( opt )
+
+if ( command == 'help' || command == '-h' || command == '-?' ) {
+    usage()
+    console.log( chalk.yellow( 'To create a new SMK project:     ' ) + chalk.blueBright( `${ exec } create [name]` ) )
+    console.log( chalk.yellow( 'To modify an SMK project config: ' ) + chalk.blueBright( `${ exec } ui [port]` ) )
+    return 1
 }
-// if the first arg exists, and it says 'create', then we should attempt to launch the cli project creator
-else if(args[0] && args[0] === 'create')
-{
-    cli.launch(args);
-}
+
+if ( !command )
+    console.log( chalk.red( 'No command specified' ) )
 else
-{
-    console.log(chalk.red('No command specified...'));
-    console.log(chalk.yellow("If you wish to create a new SMK project, please type 'smk create'"));
-    console.log(chalk.yellow("If you wish to modify an SMK project config, please type 'smk ui [port]'"));
+    console.log( chalk.red( `Unknown command specified: "${ command }"` ) )
+
+usage()
+return 1
+
+function usage() {
+    console.log( 'Usage: ' + chalk.blueBright( `${ exec } create|ui|help` ) )
 }
---
+++ smk-create/index.js
@@ -0,0 +1,176 @@
+const chalk = require( 'chalk' )
+const figlet = require( 'figlet' )
+const fs = require( 'fs' )
+const path = require( 'path' )
+const inquirer = require( 'inquirer' )
+const shell = require( 'shelljs' )
+
+module.exports = async function ( args ) {
+    // console.log( args )
+    var name = args._.shift()
+    var baseDir = args.base || args.b || process.cwd()
+
+    console.log(chalk.yellow(figlet.textSync('Simple Map Kit', { horizontalLayout: 'full' })));
+    console.log('');
+    console.log(chalk.blue('Welcome to the SMK application creation tool!'));
+    console.log(chalk.blue('A application skeleton will be created for you at the current directory.'));
+    console.log(chalk.blue('But first, please answer some questions about your new SMK application.'));
+    console.log('');
+
+    const app = await inquireAppInfo( name, baseDir );
+    app.tool = app.tools.reduce( function ( acc, t ) { acc[ t ] = true; return acc }, {} )
+
+    app.absoluteDir = path.resolve( baseDir, app.name )
+    if ( fs.existsSync( app.absoluteDir ) ) {
+        console.log( `Project directory ${ app.absoluteDir } already exists` )
+        return
+    }
+
+    app.createdDate = ( new Date() ).toISOString()
+    app.smkDistPath = './node_modules/@qqnluaq/smk/dist/smk.js'
+
+    console.log()
+    console.log( chalk.green( `Creating application directory ${ app.absoluteDir.replace( process.cwd(), '.' ) } with template "${ app.template }"...` ) )
+    require( './template-' + app.template )( app, app.absoluteDir )
+
+    installApplication()
+
+    console.log(chalk.green('Your application has been created.'));
+
+    const viewApp = await inquirer.prompt( [ {
+        name: 'answer',
+        type: 'list',
+        message: 'Would you like to view the application now?',
+        choices: [ 'Yes', 'No' ],
+        default: 'Yes'
+    } ] )
+    if ( viewApp.answer == 'Yes' )
+        viewApplication()
+
+    console.log()
+    console.log( chalk.green( 'To view this application:' ) )
+    console.log( chalk.blueBright( `  cd ${ app.name }` ) )
+    console.log( chalk.blueBright( '  npm run browse' ) )
+    console.log()
+    console.log( chalk.green( 'To modify the configuration for this application:' ) )
+    console.log( chalk.blueBright( `  cd ${ app.name }` ) )
+    console.log( chalk.blueBright( '  smk ui' ) )
+    console.log()
+
+    function installApplication() {
+        console.log()
+        console.log( chalk.green( 'Installing application dependencies...' ) )
+
+        shell.cd( app.absoluteDir )
+
+        const npmInstall = shell.exec( 'npm install' )
+        if ( npmInstall.code != 0 ) {
+            console.log( chalk.red( 'npm install failed' ) )
+            throw Error( 'Application install failed' )
+        }
+    }
+
+    function viewApplication() {
+        console.log()
+        console.log( chalk.green( 'Launching application...' ) )
+
+        shell.cd( app.absoluteDir )
+
+        shell.exec( 'npm run browse' )
+    }
+}
+
+async function inquireAppInfo( name, baseDir ) {
+    return inquirer.prompt( [
+        {
+            name: 'name',
+            type: 'input',
+            message: "Enter your application's name:",
+            default: name || 'smk-app', //argv._[0] || files.getCurrentDirectoryBase(),
+            validate: function( value )
+            {
+                if ( !value ) return 'A application name is required to create a new SMK application.'
+                var dir = path.resolve( baseDir, value )
+                if ( fs.existsSync( dir ) ) return `This application name can't be used as the directory ${ dir.replace( process.cwd(), '.' ) } already exists.`
+
+                return true
+            }
+        },
+        {
+            name: 'title',
+            type: 'input',
+            message: "Enter your application's title:",
+            default: 'My SMK Application'
+        },
+        {
+            name: 'description',
+            type: 'input',
+            message: "Enter a short description for your application:",
+            default: 'My Simple Map Kit application.'
+        },
+        {
+            name: 'author',
+            type: 'input',
+            message: "Enter the author's name:",
+            default: 'SMK Developer'
+        },
+        {
+            name: 'template',
+            type: 'list',
+            message: 'Select the template for your application:',
+            choices: [ 'default', 'mobile' ],
+            default: 'default'
+        },
+        {
+            name: 'viewer',
+            type: 'list',
+            message: 'Select the type of map viewer:',
+            choices: [ 'leaflet', 'esri3d' ],
+            default: 'leaflet'
+        },
+        // {
+        //     name: 'longitude',
+        //     type: 'input',
+        //     message: 'Starting Longitude:',
+        //     default: '-128.7817'
+        // },
+        // {
+        //     name: 'latitude',
+        //     type: 'input',
+        //     message: 'Starting Latitude:',
+        //     default: '53.7764'
+        // },
+        // {
+        //     name: 'zoom',
+        //     type: 'input',
+        //     message: 'Starting zoom:',
+        //     default: '5',
+        //     validate: function( value )
+        //     {
+        //         if (parseInt(value) >= 0 && parseInt(value) <= 20 ) return true;
+        //         else return 'Zoom level must be a value from 0 to 20';
+        //     }
+        // },
+        // {
+        //     name: 'basemaps',
+        //     type: 'checkbox',
+        //     message: 'Select your basemap options:',
+        //     choices: ['Streets', 'Topographic', 'NationalGeographic', 'Oceans', 'Gray', 'DarkGray', 'Imagery', 'ShadedRelief'],
+        //     default: ['Topographic', 'Imagery', 'Gray']
+        // },
+        {
+            name: 'baseMap',
+            type: 'list',
+            message: 'Select the base map:',
+            choices: [ 'Streets', 'Topographic', 'NationalGeographic', 'Oceans', 'Gray', 'DarkGray', 'Imagery', 'ShadedRelief' ],
+            default: 'Topographic'
+        },
+        {
+            name: 'tools',
+            type: 'checkbox',
+            message: 'Select the tools:',
+            choices: [ 'about', 'coordinate', 'layers', 'pan', 'zoom', 'measure', 'markup', 'scale', 'minimap', 'directions', 'location', 'select', 'identify', 'search' ],
+            default: [ 'about', 'coordinate', 'layers', 'pan', 'zoom', 'scale', 'minimap', 'identify', 'search' ]
+        }
+    ] )
+}
