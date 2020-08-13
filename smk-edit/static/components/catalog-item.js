11f6dc320d22ce1e145bbac1f30ae5fcd732bced
Benjamin Jubb <benjubb@gmail.com>
2020-08-13 16:38:21 -0700

    control item visibility directly on catalog-item

--- smk-edit/static/components/catalog-item.js
+++ smk-edit/static/components/catalog-item.js
@@ -20,11 +20,30 @@ vueComponent( import.meta.url, {
             return this.$store.getters.configHasLayer( this.itemId ) && this.$store.getters.configLayer( this.itemId ).metadataUrl
         },
         allowedEdit: function () { return !this.allowed || this.allowed.edit !== false },
-        allowedRemove: function () { return !this.allowed || this.allowed.remove !== false }
+        allowedRemove: function () { return !this.allowed || this.allowed.remove !== false },
+        isVisible: {
+            get: function () {
+                var item = this.$store.getters.configToolLayersDisplayItem( this.itemId )
+                if ( item.isVisible != null ) return item.isVisible
+                return this.$store.getters.configLayer( this.itemId ).isVisible
+            },
+            set: function ( val ) {
+                this.$store.dispatch( 'configToolLayersDisplayItem', { id: this.itemId, isVisible: val } )
+            }
+        },
     },
     methods: {
         edit: function () { this.$emit( 'edit-item', this.itemId ) },
         remove: function () { this.$emit( 'remove-item', this.itemId ) }
+    },
+    mounted: function () {
+        M.Tooltip.init( this.$refs.visibility, {
+            html: 'Initial visibility of this layer',
+            position: 'top',
+            enterDelay: 1000,
+            inDuration: 500,
+            outDuration: 100,
+        } )
     }
 } )
 
