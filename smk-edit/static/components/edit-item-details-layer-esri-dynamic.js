0ff3906f87cd48644af5946c246e9f1bca0028f2
Michael Nelson <michael.nelson@gov.bc.ca>
2022-04-14 15:48:39 -0700

    MPCM layers can show labels (#94)
    * MPCM layers can show labels
    
    This updates the layer editor for MPCM layers. When an MPCM layer has a non-empty "labelingInfo" value, we add a checkbox to show label information. This then sets the value of the "showLabels" property of the "drawingInfo" property of dynamic layers in smk-config.json.

--- smk-edit/static/components/edit-item-details-layer-esri-dynamic.js
+++ smk-edit/static/components/edit-item-details-layer-esri-dynamic.js
@@ -4,6 +4,49 @@ export default importComponents( [
 ] ).then( function () {
     return vueComponent( import.meta.url, {
         props: [ 'itemId' ],
+        computed: {
+            hasLabels: function() {
+                const layer = this.$store.getters.configLayer( this.itemId );
+                if (layer && layer.dynamicLayers) {
+                    for (const dynamicLayer of layer.dynamicLayers) {
+                        const dynamicJson = JSON.parse(dynamicLayer);
+                        if(dynamicJson.drawingInfo && dynamicJson.drawingInfo.labelingInfo) {
+                            return true;
+                        }
+                    };
+                }
+                return false;
+            },
+            showLabels: {
+                get: function () {
+                    const layer = this.$store.getters.configLayer( this.itemId );
+                    if (layer && layer.dynamicLayers) {
+                        for (const dynamicLayer of layer.dynamicLayers) {
+                            const dynamicJson = JSON.parse(dynamicLayer);
+                            if(dynamicJson.drawingInfo 
+                                && dynamicJson.drawingInfo.labelingInfo 
+                                && dynamicJson.drawingInfo.showLabels) {
+                                    return true;
+                            }
+                        }
+                    }
+                    return false;
+                },
+                set: function ( val ) {
+                    const layer = this.$store.getters.configLayer( this.itemId );
+                    if (layer && layer.dynamicLayers) {
+                        for (let i = 0; i < layer.dynamicLayers.length; i++) {
+                            const dynamicJson = JSON.parse(layer.dynamicLayers[i]);
+                            if(dynamicJson.drawingInfo && dynamicJson.drawingInfo.labelingInfo) {
+                                dynamicJson.drawingInfo.showLabels = val;
+                                layer.dynamicLayers[i] = JSON.stringify(dynamicJson);
+                            }
+                        }
+                        this.$store.dispatch( 'configLayer', layer );
+                    }
+                }
+            }
+        },
         mounted: function () {
         },
         updated: function () {
